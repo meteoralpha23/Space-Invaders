4b9a573c74ed364c751657708b9b3bd642dba512
meteoralpha23 <luckycreed123@gmail.com>
2024-07-27 11:30:54 +0530

    Update main.cpp

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -4,7 +4,7 @@ int main()
 {
     // Define the video mode (dimensions)
     sf::VideoMode videoMode = sf::VideoMode(1020,780 );
-
+    
     
     sf::RenderWindow window(videoMode, "This is my first SFML Window");
     while (window.isOpen()) {
@@ -18,6 +18,20 @@ int main()
 
         // Clear the window
         window.clear(sf::Color::Cyan);
+        sf::CircleShape circle(50); // Radius 50
+        circle.setFillColor(sf::Color::Red);
+        circle.setPosition(400, 300); // Set position
+        window.draw(circle);
+
+        sf::RectangleShape rect(sf::Vector2f(100, 50));
+        rect.setFillColor(sf::Color::Blue);
+        rect.setPosition(600, 400);
+        window.draw(rect);
+
+        sf::RectangleShape line(sf::Vector2f(100, 200));
+        line.rotate(35);
+        line.setPosition(200, 470);
+        window.draw(line);
 
         // Display whatever you draw
         window.display();
