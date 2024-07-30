6334ee59cc6d374384f6dca6cbb8719f7fc43b57
meteoralpha23 <luckycreed123@gmail.com>
2024-07-30 20:13:38 +0530

    Commiting

---
+++ Space-Invaders/Source/GameService.cpp
@@ -0,0 +1,36 @@
+#include "D:\Outscal\Space-Invaders\Space-Invaders\Header\GameService.h"
+
+
+
+void GameService::Initialize()
+{
+}
+
+void GameService::Destroy()
+{
+}
+
+GameService::GameService()
+{
+}
+
+GameService::~GameService()
+{
+}
+
+void GameService::Ignite()
+{
+}
+
+void GameService::update()
+{
+}
+
+void GameService::render()
+{
+}
+
+bool GameService::isRunning()
+{
+	return false;
+}
